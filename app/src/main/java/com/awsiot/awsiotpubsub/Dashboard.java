51971cad71b68ffd4398ae96b81060c79255ff92
birensharma <birensharma@st.niituniversty.in>
2019-03-26 12:13:29 +0530

    push

--- app/src/main/java/com/awsiot/awsiotpubsub/Dashboard.java
+++ app/src/main/java/com/awsiot/awsiotpubsub/Dashboard.java
@@ -37,7 +37,7 @@ import java.util.UUID;
 
 public class Dashboard extends Fragment {
 
-///hhhelo
+///hhhelogggggggooooooooo
     static final String LOG_TAG = Dashboard.class.getCanonicalName();
 
     @Override
